ea2d9a4669d27935727e0aad423798aa50e5f447
lorenascimentoo <lorenanascimento28@gmail.com>
2021-09-24 14:36:49 -0300

    Atualizacao da classe Cadastro[sem validacao] e ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ATP10
 Repositório criado para a resolução a atividade prática 10 da academia técnica.
+
+- Exercicio 1.
+_ Crie um programa em linguagem Java que solicita ao usuário que digite a idade, armazene em uma variável do tipo byte e imprima uma mensagem para idade maior que 25 anos, outra mensagem para idade menor que 25 e uma outra mensagem se idade igual a 25 anos.
--- src/modulo1/Cadastro.java
+++ src/modulo1/Cadastro.java
@@ -6,29 +6,24 @@ import java.util.Scanner;
  * Cadastro
  */
 public class Cadastro {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-            System.out.println("========== VERIFICAÇÃO DA IDADE ==========");
+        System.out.println("========== VERIFICAÇÃO DA IDADE ==========");
 
-            Byte idade;
+        System.out.print("Informe sua idade: ");
+        Byte idade = Byte.parseByte(sc.nextLine());
 
-            do{
-                System.out.print("Informe sua idade: ");
-                idade = Byte.parseByte(sc.nextLine());
-
-                if(idade<0){
-                    System.out.println("Não aceito valor menor que zero!");
-                }
-            } while(idade<0);
-            
-            if(idade <25){
-                System.out.println("Sua idade é menor que 25 anos!");
-            } else if(idade==25){
-                System.out.println("Sua idade é 25 anos!");
-            } else {
-                System.out.println("Sua idade é maior que 25 anos!");
-            }
+        if (idade < 0) {
+            System.out.println("Idade inválida!");
+            System.out.println("Valor informado menor que zero!");
+        } else if (idade < 25) {
+            System.out.println("Sua idade é menor que 25 anos!");
+        } else if (idade == 25) {
+            System.out.println("Sua idade é 25 anos!");
+        } else {
+            System.out.println("Sua idade é maior que 25 anos!");
         }
-    
+    }
+
 }
